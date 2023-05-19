f7d11b7e2b2fa2eb6bbbf0e9fa4477741093faf1
Jefferson Alves <jeeffsantoos@outlook.com>
2023-05-18 22:42:47 -0300

    feat: atualizar dados

---
+++ atualizarDados.py
@@ -0,0 +1,65 @@
+import sqlite3
+
+def atualizarNomeCliente(id , nome):
+    try:
+        TABLE_NAME = "Clientes"
+
+        with sqlite3.connect("CRUD_AP2.db") as connection:
+            cursor = connection.cursor()
+
+            cursor.execute(
+                f'UPDATE {TABLE_NAME} '
+                'SET nome = ? '
+                'WHERE ClienteId = ?',
+                (nome, id)
+            )
+
+            connection.commit()
+            print("Cliente atualizado com sucesso.")
+            
+    except Exception as e:
+        print("Erro ao atualizar o cliente: ", str(e))
+
+def atualizarCpfCliente(id , cpf):
+    try:
+        TABLE_NAME = "Clientes"
+
+        with sqlite3.connect("CRUD_AP2.db") as connection:
+            cursor = connection.cursor()
+
+            cursor.execute(
+                f'UPDATE {TABLE_NAME} '
+                'SET cpf = ? '
+                'WHERE ClienteId = ?',
+                (cpf, id)
+            )
+
+            connection.commit()
+            print("Cliente atualizado com sucesso.")
+            
+    except Exception as e:
+        print("Erro ao atualizar o cliente: ", str(e))
+
+def atualizarTelefoneCliente(id, telefone):
+    try:
+        TABLE_NAME = "Clientes"
+
+        with sqlite3.connect("CRUD_AP2.db") as connection:
+            cursor = connection.cursor()
+
+            cursor.execute(
+                f'UPDATE {TABLE_NAME} '
+                'SET telefone = ? '
+                'WHERE ClienteId = ?',
+                (telefone, id)
+            )
+
+            connection.commit()
+            print("Cliente atualizado com sucesso.")
+            
+    except Exception as e:
+        print("Erro ao atualizar o cliente: ", str(e))
+        
+atualizarNomeCliente(1, 'teste')
+atualizarTelefoneCliente(1, 'teste')
+atualizarCpfCliente(1, 123)
