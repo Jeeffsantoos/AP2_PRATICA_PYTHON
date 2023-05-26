1089d4466c5bdc8e1a70d1f674d97fcbc7ace44e
Jefferson Alves <jeeffsantoos@outlook.com>
2023-05-26 16:46:50 -0300

    feat: Verificando se os ID's existem

--- deletarCampo.py
+++ deletarCampo.py
@@ -8,18 +8,20 @@ def deletarCliente(id):
         with sqlite3.connect("CRUD_AP2.db") as connection:
             cursor = connection.cursor()
 
-            cursor.execute(
-                # Está vendo o id?
-                # era pra ele estar como ClienteId (Nome que demos para ele)
-                f"DELETE FROM {TABLE_NAME} WHERE ClienteId = ?",
-                (id,),
-            )
+            # Verifica se o ID existe na tabela antes de executar a exclusão
+            cursor.execute(f"SELECT COUNT(*) FROM {TABLE_NAME} WHERE ClienteId = ?", (id,))
+            result = cursor.fetchone()
 
-            connection.commit()
-            print("Cliente deletado com sucesso.")
+            if result[0] > 0:  # Se o ID existe na tabela
+                cursor.execute(f"DELETE FROM {TABLE_NAME} WHERE ClienteId = ?", (id,))
+                connection.commit()
+                print("\nCliente deletado com sucesso.")
+            else:
+                print("\nID do cliente não encontrado.")
+                
     except Exception as e:
         print("Erro ao deletar o cliente: ", str(e))
-
+        
 # Deleta a motocicleta no banco de dados
 def deletarMotocicleta(id):
     try:
@@ -28,13 +30,17 @@ def deletarMotocicleta(id):
         with sqlite3.connect("CRUD_AP2.db") as connection:
             cursor = connection.cursor()
 
-            cursor.execute(
-                f"DELETE FROM {TABLE_NAME} WHERE MotoId = ?",
-                (id,),
-            )
+            # Verifica se o ID existe na tabela antes de executar a exclusão
+            cursor.execute(f"SELECT COUNT(*) FROM {TABLE_NAME} WHERE MotoId = ?", (id,))
+            result = cursor.fetchone()
 
-            connection.commit()
-            print("Motocicleta deletada com sucesso.")
+            if result[0] > 0:  # Se o ID existe na tabela
+                cursor.execute(f"DELETE FROM {TABLE_NAME} WHERE MotoId = ?", (id,))
+                connection.commit()
+                print("\nMotocicleta deletada com sucesso.")
+            else:
+                print("\nID da moto não encontrado.")
+                
     except Exception as e:
         print("Erro ao deletar a motocicleta: ", str(e))
 
@@ -45,13 +51,16 @@ def deletarVenda(id):
 
         with sqlite3.connect("CRUD_AP2.db") as connection:
             cursor = connection.cursor()
-            
-            cursor.execute(
-                f"DELETE FROM {TABLE_NAME} WHERE VendaId = ?",
-                (id,),
-            )
 
-            connection.commit()
-            print("Venda deletada com sucesso.")
+            # Verifica se o ID existe na tabela antes de executar a exclusão
+            cursor.execute(f"SELECT COUNT(*) FROM {TABLE_NAME} WHERE VendaId = ?", (id,))
+            result = cursor.fetchone()
+
+            if result[0] > 0:  # Se o ID existe na tabela
+                cursor.execute(f"DELETE FROM {TABLE_NAME} WHERE VendaId = ?", (id,))
+                connection.commit()
+                print("\Venda deletada com sucesso.")
+            else:
+                print("\nID da venda não encontrado.")
     except Exception as e:
         print("Erro ao deletar a venda: ", str(e))
